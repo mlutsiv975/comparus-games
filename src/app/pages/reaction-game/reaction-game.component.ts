3675693b92bcb169d78d5dd359a0c8c91a17d351
mlutsiv <mlutsiv975@gmail.com>
2024-11-27 00:56:07 +0200

    add prettier ignore file

--- src/app/pages/reaction-game/reaction-game.component.ts
+++ src/app/pages/reaction-game/reaction-game.component.ts
@@ -1,20 +1,18 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {GameService} from '../../services/reaction-game/game-service/game.service';
-import {ScoreBoardComponent} from '../../components/reaction-game/score-board/score-board.component';
-import {GameBoardColComponent} from '../../components/reaction-game/game-board-col/game-board-col.component';
-import {
-  ReactionGameModalComponent
-} from '../../components/reaction-game/reaction-game-modal/reaction-game-modal.component';
-import {GameState} from '../../utils/reaction-game/game-state';
-import {Subject, takeUntil} from 'rxjs';
-import {REACTION_GAME_CONFIG} from '../../utils/reaction-game/config';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { GameService } from '../../services/reaction-game/game-service/game.service';
+import { ScoreBoardComponent } from '../../components/reaction-game/score-board/score-board.component';
+import { GameBoardColComponent } from '../../components/reaction-game/game-board-col/game-board-col.component';
+import { ReactionGameModalComponent } from '../../components/reaction-game/reaction-game-modal/reaction-game-modal.component';
+import { GameState } from '../../utils/reaction-game/game-state';
+import { Subject, takeUntil } from 'rxjs';
+import { REACTION_GAME_CONFIG } from '../../utils/reaction-game/config';
 
 @Component({
   selector: 'app-reaction-game',
   imports: [FormsModule, ScoreBoardComponent, GameBoardColComponent, ReactionGameModalComponent],
   templateUrl: './reaction-game.component.html',
-  styleUrl: './reaction-game.component.scss'
+  styleUrl: './reaction-game.component.scss',
 })
 export class ReactionGameComponent implements OnInit, OnDestroy {
   rows = Array.from({ length: REACTION_GAME_CONFIG.boardRows }, (_, i) => i);
@@ -31,19 +29,21 @@ export class ReactionGameComponent implements OnInit, OnDestroy {
   constructor(private gameService: GameService) {}
 
   ngOnInit() {
-    this.gameService.getGameState$()
+    this.gameService
+      .getGameState$()
       .pipe(takeUntil(this.destroy$))
-      .subscribe(state => {
-      this.currentGameState = state;
-      this.buttonLabel = state === GameState.Ready ? 'Start' : 'Restart';
-      this.showModal = state === GameState.Finished;
-    });
+      .subscribe((state) => {
+        this.currentGameState = state;
+        this.buttonLabel = state === GameState.Ready ? 'Start' : 'Restart';
+        this.showModal = state === GameState.Finished;
+      });
 
-    this.gameService.getScore$()
+    this.gameService
+      .getScore$()
       .pipe(takeUntil(this.destroy$))
-      .subscribe(score => {
-      this.score = score;
-    });
+      .subscribe((score) => {
+        this.score = score;
+      });
   }
 
   startOrResetGame() {
